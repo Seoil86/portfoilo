9f9ebb734150d58e2b43b2444fa960e5ffd8ebd1
Seoil86 <74699239+Seoil86@users.noreply.github.com>
2020-12-14 09:41:36 +0900

    up

--- js/script.js
+++ js/script.js
@@ -26,3 +26,19 @@ setInterval(() => {
 }, 1000);
 
 
+
+
+
+$('.film .film-slide .scene a img').click(function(){ // n번 버튼을 클릭했을 때
+$(this).addClass('on'); // n번 버튼 활성화
+$(this).parent().siblings().find('img').removeClass('on'); // n번 버튼을 제외한 나머지 버튼 비활성화
+var $idx = $(this).parent('a').index();
+$('.screen-cont > div').eq($idx).css('display','block'); // n번 컨텐츠 활성화
+$('.screen-cont > div').eq($idx).siblings().css('display','none'); // n번 컨텐츠 외 나머지 컨텐츠 비활성화
+
+return false; // a태그 무효화
+});
+
+// 처음 창을 열었을 때, 기본으로 세팅되어 있는 경우
+$('.screen-cont1').addClass('on');
+$('.screen-cont1').css('display','block');
